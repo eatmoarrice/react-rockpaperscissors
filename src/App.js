aabe09e9b772c643ce792ffbafa3672df38d3947
unknown <xvinh.ir@gmail.com>
2020-05-26 16:26:20 +0700

    add more functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ChoiceCard from "./components/ChoiceCard";
-import List from "./components/List";
+import NameForm from "./components/NameForm";
+// import List from "./components/List";
 
-const choices = {
-  rock:
-    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png",
+const CHOICES = {
+    scissors: {
+        name: "scissors",
+        url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png",
+    },
+    paper: {
+        name: "paper",
+        url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+    },
+    rock: {
+        name: "rock",
+        url: "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+    },
 };
 
+let history = [];
+
 function App() {
-  return (
-    <div className="App">
-      <ChoiceCard title="You" winner={true} imgURL={choices.rock} />
-      <ChoiceCard title="Computer" winner={false} imgURL={choices.scissors} />
-    </div>
-  );
+    // let htmlResult;
+    const [flawless, setflawless] = useState("");
+    let user = "";
+    let [userC, setuserC] = useState({});
+    let [computerC, setcomputerC] = useState({});
+    let [result, setresult] = useState("");
+    let [previousWinner, setPreviousWinner] = useState(null);
+
+    let play = (userChoice) => {
+        // userChoose(userChoice);
+        let uChoice = userChoice;
+        let cChoice = computerChoice();
+        let tempResult = getResult(uChoice, cChoice);
+        setflawless(checkHistory(tempResult));
+        if (tempResult === "Victory!") {
+            setPreviousWinner("You");
+        } else if (tempResult === "Defeat!") {
+            setPreviousWinner("Computer");
+        } else {
+            setPreviousWinner("Tie");
+        }
+        setuserC(CHOICES[uChoice]);
+        setcomputerC(CHOICES[cChoice]);
+        setresult(tempResult);
+
+        // htmlResult = result;
+        // console.log("result:", userC.name, computerC.name, getResult());
+    };
+    let checkHistory = (newResult) => {
+        let prettyResult = "";
+        history.unshift(newResult);
+        console.log(history);
+        if (history[0] === "Victory!" && history[1] === "Victory!" && history[2] === "Victory!") {
+            prettyResult = "Flawless Victory!";
+        }
+        return prettyResult;
+    };
+    let computerChoice = () => {
+        let choiceArray = Object.keys(CHOICES);
+        let compChoice = CHOICES[choiceArray[Math.floor(Math.random() * choiceArray.length)]];
+        return compChoice.name;
+    };
+
+    // let userChoose = (userChoice) => {
+    //   setuserC(CHOICES[userChoice]);
+    //   let choiceArray = Object.keys(CHOICES);
+    //   setcomputerC(
+    //     CHOICES[choiceArray[Math.floor(Math.random() * choiceArray.length)]]
+    //   );
+    // };
+
+    let getResult = (u, c) => {
+        let result;
+        if (u === "rock") {
+            result = c === "scissors" ? "Victory!" : "Defeat!";
+        }
+        if (u === "paper") {
+            result = c === "rock" ? "Victory!" : "Defeat!";
+        }
+        if (u === "scissors") {
+            result = c === "paper" ? "Victory!" : "Defeat!";
+        }
+        if (u === c) result = "Tie game!";
+        // if (result === "Victory!") {
+        //   setPreviousWinner("You");
+        // } else if (result === "Defeat!") {
+        //   setPreviousWinner("Computer");
+        // } else {
+        //   setPreviousWinner("Tie");
+        // }
+        return result;
+    };
+
+    return (
+        <div className="App">
+            {/* {user === "" ? <NameForm></NameForm> : <h1>WTF</h1>} */}
+            <ChoiceCard title="You" choice={userC} previousWinner={previousWinner} />
+            {/* <div> */}
+            <div className="d-flex flex-column justify-content-center mid">
+                <button className="btn btn-primary my-3" onClick={() => play("rock")}>
+                    Rock
+                </button>
+                <button className="btn btn-primary my-3" onClick={() => play("paper")}>
+                    Paper
+                </button>
+                <button className="btn btn-primary my-3" onClick={() => play("scissors")}>
+                    Scissors
+                </button>
+                <div>Result is: {result}</div>
+                <div id="flawless">{flawless}</div>
+            </div>
+
+            {/* </div> */}
+            <ChoiceCard title="Computer" choice={computerC} previousWinner={previousWinner} />
+            <div className="history my-auto">
+                {history.map((item) => (
+                    <div>{item}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -1,23 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 
-// export default function Box(props) {
+const DEFAULT_IMG = "http://www.thewateringhole.co.uk/wp-content/uploads/2012/12/play.png";
+
+export default function ChoiceCard(props) {
+    const won = props.title === props.previousWinner;
+    let className;
+    const hasPreviousGame = props.previousWinner === "Computer" || props.previousWinner === "You";
+    if (hasPreviousGame) {
+        className = won ? "winner" : "loser";
+    }
+
+    let prompt;
+    if (won) {
+        prompt = "Won!";
+        className = won ? "winner" : "loser";
+    } else if (props.previousWinner === "Tie") {
+        prompt = "It's a tie!";
+    } else if (props.previousWinner === null) {
+        prompt = "Start";
+    } else {
+        prompt = "Defeated!";
+    }
+
+    return (
+        <div className={`choice-card ${className}`}>
+            <h1>{props.title}</h1>
+            <img src={props.choice.url || DEFAULT_IMG} alt={props.title} />
+            <h3>{prompt}</h3>
+        </div>
+    );
+}
+
+// export default function ChoiceCard(props) {
 //     return (
-//         <div className={`choice-card ${props.winner ? 'winner' : 'loser'}`}>
+//         <div className={`choice-card ${props.winner}`}>
 //             <h1>{props.title}</h1>
-//             <img src={props.imgURL} alt={props.title}/>
-//             <h3>{props.winner ? 'Win' : 'Loss'}</h3>
+//             <img src={props.choice.url} alt={props.choice.name} />
+//             <h3>{props.winner}</h3>
 //         </div>
-//     )
+//     );
 // }
 
-export default class ChoiceCard extends Component {
-  render() {
-    return (
-      <div className={`choice-card ${this.props.winner ? "winner" : "loser"}`}>
-        <h1>{this.props.title}</h1>
-        <img src={this.props.imgURL} alt={this.props.title} />
-        <h3>{this.props.winner ? "Win" : "Loss"}</h3>
-      </div>
-    );
-  }
-}
+// export default class ChoiceCard extends Component {
+//   render() {
+//     return (
+//       <div className={`choice-card ${this.props.winner ? "winner" : "loser"}`}>
+//         <h1>{this.props.title}</h1>
+//         <img src={this.props.imgURL} alt={this.props.title} />
+//         <h3>{this.props.winner ? "Win" : "Loss"}</h3>
+//       </div>
+//     );
+//   }
+// }
